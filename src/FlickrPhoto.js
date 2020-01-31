ec5d7ba0ec4d6f83bcc91a001c50f4c56e7f3351
flexiblefactory <hal@Hals-MacBook-Pro.local>
2020-01-31 11:00:19 +0000

    Remove inline CSS

--- src/FlickrPhoto.js
+++ src/FlickrPhoto.js
@@ -1,43 +1,24 @@
 import React from 'react';
+import { observer } from 'mobx-react'
 
 const FlickrPhoto = ({ src, title, media, description, tags, author }) =>
-  <div style={{
-    display: 'inline-table',
-    backgroundColor: 'black',
-    width: '500px',
-    height: '500px',
-    maxWidth:'100%',
-    backgroundImage: `url(${src})`,
-    backgroundRepeat: 'no-repeat',
-    backgroundPosition: 'center',
-    backgroundSize: 'contain',
-    color: 'white',
-    position: 'relative',
-    boxSizing:'border-box',
-    marginBottom:'20px',
-    marginRight:'20px'
-  }}>
-    <div style={{
-      display: 'inline-table',
-      backgroundColor: 'rgba(0,0,0,.5)',
-      maxWidth:'100%',
-width:'100%',
-      position: 'absolute',
-      bottom: '0',
-      left:0,
-      padding:'20px',
-      boxSizing:'border-box',
-      wordWrap: 'break-word',
-overflowWrap: 'break-word',
-wordBreak: 'break-all'
-          }}>
+  <div className='flickr-photo' style={{ backgroundImage: `url(${src})` }}>
+    <div className='title-box'>
       {title}
     </div>
   </div>
 
-{/* <i>by {author}</i>
+{
+  /* 
+  
+  see notes in readme
+  
+  <i>by {author}</i>
 <p dangerouslySetInnerHTML={{ __html: description }} ></p>
 <h2>Tags:</h2>
-{tags} */}
+{tags} 
 
-export default FlickrPhoto
+*/
+}
+
+export default observer(FlickrPhoto)
